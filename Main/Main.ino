0ba9ee6db9b587ae31b9729cb86e3b72151b525e
keatonshall <keatonshall314@gmail.com>
2025-05-28 10:27:15 -0500

    Outline of functions and flight mode transitions

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,3 +1,11 @@
+#include <Servo.h>
+
+double t_i, t_f = 0;
+double y_i, y_f = 0;
+double v_i, v_f = 0;
+
+double servoCommand = 0;
+
 enum FlightMode {
   LAUNCH_PAD = 0,
   THRUST = 1,
@@ -8,13 +16,68 @@ enum FlightMode {
 
 enum FlightMode mode = LAUNCH_PAD;
 
+Servo servo;
+
 
 void setup() {
-  // put your setup code here, to run once:
+  //Initialize hardware
+  servo.attach(0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
+  sample();
+
+  //Mange flight states
+  switch(mode){
+    case LAUNCH_PAD:
+      if (v_f > 1)
+        mode++;
+    case THRUST:
+      //Add accelerometer data
+    case COAST:
+      //Add airbrake control for apogge
+      if (v_f < 0)
+        mode++;
+    case RECOVER:
+      //Add airbrake control for flight time
+      if(v_f > -0.5 && v_f < 0.5)
+        mode++;
+    case LAND:
+      servoCommand = 0;
+  }
+
+  updateServo();
+  log();
 }
 
+void updateServo(){
+  servo.write(servoCommand);
+}
+
+void log(){
+  //log raw sensor inputs, filtered/calculated values, flight mode, and servo output to sd card
+}
+
+void sample(){
+  updateAltitude();
+  updateVerticalVelocity();
+}
+
+//Returns the altitude in meters
+void updatAltitude(){
+  //TODO: fuse barometer and accelerometer data using kalman filter
+
+  //Shift back samples
+  y_i = y_f;
+  t_i = t_f;
+
+  //Update new samples
+  y_f = 0; //Use sensor here
+  t_f = millis() / 1000.;
+}
+
+//Returns vy in m/s
+void updateVerticalVelocity(){
+  v_i = v_f;
+  v_f = (y_f - y_i) / (t_i - t_f);
+}
