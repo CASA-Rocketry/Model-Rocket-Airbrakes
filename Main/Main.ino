993df0d9e39826ef6383bb69b242704d65679746
keatonshall <keatonshall314@gmail.com>
2025-05-28 09:56:27 -0500

    FlightMode state enums

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,9 +1,20 @@
+enum FlightMode {
+  LAUNCH_PAD = 0,
+  THRUST = 1,
+  COAST = 2,
+  RECOVERY = 3,
+  LAND = 4
+}
+
+enum FlightMode mode = LAUNCH_PAD;
+
+
 void setup() {
   // put your setup code here, to run once:
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
+
