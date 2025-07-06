a57da6e999b5f763a8a89d62f5ac0ab2fb533c78
keatonshall <keatonshall314@gmail.com>
2025-07-05 22:52:00 -0500

    Hardware test minor changes

--- HardwareTest/ServoTest/ServoTest.ino
+++ HardwareTest/ServoTest/ServoTest.ino
@@ -4,14 +4,16 @@ Servo servo;
 
 void setup() {
   // put your setup code here, to run once:
-  servo.attach(9);
-
+  servo.attach(3);
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   servo.write(0);
-  delay(100);
+  digitalWrite(2, HIGH);
+  delay(1000);
   servo.write(90);
-  delay(100);
+  digitalWrite(2, LOW);
+  delay(1000);
 }
