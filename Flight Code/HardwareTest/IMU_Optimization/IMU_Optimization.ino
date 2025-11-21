9f68aab64f353eaedc24a28da684e8653eb7c131
keatonshall <keatonshall314@gmail.com>
2025-11-20 21:18:15 -0600

    Added IMU testing sketch and edited libraries

---
+++ Flight Code/HardwareTest/IMU_Optimization/IMU_Optimization.ino
@@ -0,0 +1,13 @@
+#include "Arduino_BMI270_BMM150"
+
+float ax, ay, az;
+
+void setup() {
+  // put your setup code here, to run once:
+  IMU.begin();
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
