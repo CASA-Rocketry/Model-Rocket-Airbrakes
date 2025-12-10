b8ba6ded7c329888f8b15770ad97c2ac85501bf6
Keaton Hall <keatonshall314@gmail.com>
2025-12-10 09:46:44 -0600

    Old flight code simulation testing with updated constants

--- [v1] Flight Code/Main/TrajectoryEstimation.ino
+++ [v1] Flight Code/Main/TrajectoryEstimation.ino
@@ -1,40 +1,41 @@
 #include <cmath>
 
 #define BURNOUT_MASS 0.617
-#define GRAVITY 9.81
-#define ROCKET_AREA 0.2463
-#define AIR_DENSITY 1.2
+#define GRAVITY 9.810245
+#define ROCKET_AREA 0.002463
+#define AIR_DENSITY 1.18
 
 //Uses the current servo deployment to calculate the new drag coefficient
 float getDragCoefficient(){
-  return 0.8 + getServoDeployment() * 0.5;
+  return 0.6 + 0;//getServoDeployment() * 0.8;
 }
 
 //Returns estimated apogee in meters
-// float getApogeeEstimate(){
-//   float apogeeEstimate;
-//   float k = 0.5 * getDragCoefficient() * ROCKET_AREA * AIR_DENSITY;
-//   float logArg = (k * getVEstimate() * getVEstimate()) / (BURNOUT_MASS * GRAVITY) + 1;
-//   if(logArg <= 0)
-//     apogeeEstimate = getYEstimate();
-//   else{
-//     float deltaY = (BURNOUT_MASS/(2*k)) * std::log(logArg);
-//     apogeeEstimate =  getYEstimate() + deltaY;//Insert math here
-//   }
-//   apoggeeEstiamte = getYEstimate()
-//   logLine[10] = String(apogeeEstimate);
-//   return apogeeEstimate;
-// }
-
 float getApogeeEstimate(){
   float apogeeEstimate;
   float k = 0.5 * getDragCoefficient() * ROCKET_AREA * AIR_DENSITY;
-
-  float deltaY = (0.5 * BURNOUT_MASS * getVEstimate() * getVEstimate()) / (BURNOUT_MASS * GRAVITY + k * getVEstimate() * getVEstimate());
-  apogeeEstimate = getYEstimate() + deltaY;
+  float logArg = (k * getVEstimate() * getVEstimate()) / (BURNOUT_MASS * GRAVITY) + 1;
+  if(logArg <= 0)
+    apogeeEstimate = getYEstimate();
+  else{
+    float deltaY = (BURNOUT_MASS/(2*k)) * std::log(logArg);
+    apogeeEstimate =  getYEstimate() + deltaY;//Insert math here
+  }
+  //apogeeEstiamte = getYEstimate()
   logLine[10] = String(apogeeEstimate);
+
   return apogeeEstimate;
 }
+
+// float getApogeeEstimate(){
+//   float apogeeEstimate;
+//   float k = 0.5 * getDragCoefficient() * ROCKET_AREA * AIR_DENSITY;
+
+//   float deltaY = (0.5 * BURNOUT_MASS * getVEstimate() * getVEstimate()) / (BURNOUT_MASS * GRAVITY + k * getVEstimate() * getVEstimate());
+//   apogeeEstimate = getYEstimate() + deltaY;
+//   logLine[10] = String(apogeeEstimate);
+//   return apogeeEstimate;
+// }
 
 //Returns estiamted flight time in seconds
 float getTimeEstimate(){
