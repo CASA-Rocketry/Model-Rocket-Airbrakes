9470916f4917648a443dc2b2a295589915a5432a
Keaton Hall <keatonshall314@gmail.com>
2025-12-16 10:19:36 -0600

    Binary search deployment calculation

---
+++ Flight Code/src/control.cpp
@@ -0,0 +1,28 @@
+#include "config.h"
+#include "constants.h"
+#include "Airbrake/Airbrake.h"
+
+//Computes apogee from given altitude, velocity, and cd
+//The rest of the constants are pulled from the config
+double getApogee(double y, double v, double cd){
+    double k = 0.5 * cd * config::ROCKET_AREA_METERS_SQUARED * config::AIR_DENSITY_KG_PER_METERS_CUBED;
+    double logArg = (k * v * v) / (config::BURNOUT_MASS_KG * constants::physics::GRAVITY);
+    return y + std::log(logArg) * config::BURNOUT_MASS_KG / (2 * k);
+}
+
+//Numerically solves for cd to reach target apogee in config
+//Computes 
+double computeDeployment(double y, double v){
+    //Check bounds 
+    if(getApogee(y, v, Airbrake::computeCD(0)) < config::TARGET_APOGEE_METERS)
+        return 0;
+    else if(getApogee(y, v, Airbrake::computeCD(1)) > config::TARGET_APOGEE_METERS)
+        return 1;
+
+    double addition, deployment = 0;
+    for(int digits = 1; digits <= 6; digits++){
+        addition = 1 >> digits; 
+        if(getApogee(y, v, Airbrake::computeCD(deployment + addition)) > config::TARGET_APOGEE_METERS)
+            deployment += addition;
+    }
+}
