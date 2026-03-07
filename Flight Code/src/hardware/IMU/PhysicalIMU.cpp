4bf47e1c9e0aa3d814ad47a24e0f608ff2c4e904
keatonshall <keatonshall314@gmail.com>
2026-03-07 17:51:26 -0600

    Calibration moved to main flight code and clibration data txt

--- Flight Code/src/hardware/IMU/PhysicalIMU.cpp
+++ Flight Code/src/hardware/IMU/PhysicalIMU.cpp
@@ -13,104 +13,160 @@ void PhysicalIMU::initialize(UI& ui){
     orientationIMU.setAxisRemap(Adafruit_BNO055::REMAP_CONFIG_P8);
     orientationIMU.setAxisSign(Adafruit_BNO055::REMAP_SIGN_P7);
     
+    //adafruit_bno055_offsets_t orientationOffset; //not using mag, but including anyway
+    orientationIMU.setSensorOffsets({26, -42, 78, -11, 58, -2, -2, 0, 0, 1000, 1000});
+
+    //adafruit_bno055_offsets_t accelerationOffset{34, -41, 75, 467, -55, 25, -1, -2, 1, 1000, 1000}; //not using mag, but including anyway
 
     //Setup accelerationIMU
     accelerationIMU.setMode(OPERATION_MODE_CONFIG);
     accelerationIMU.write8(Adafruit_BNO055::adafruit_bno055_reg_t::BNO055_PAGE_ID_ADDR, 0x01);
+    delay(20); //Probably not needed
     accelerationIMU.write8(Adafruit_BNO055::adafruit_bno055_reg_t::ACC_CONFIG, 0x13); //Write acc config (normal mode = 000, 125hz = 100, 16g range = 11)
+    delay(20); //Probably not needed
     accelerationIMU.write8(Adafruit_BNO055::adafruit_bno055_reg_t::BNO055_PAGE_ID_ADDR, 0x00);
-
+    delay(20); //Probably not needed
     accelerationIMU.setAxisRemap(Adafruit_BNO055::REMAP_CONFIG_P8);
     accelerationIMU.setAxisSign(Adafruit_BNO055::REMAP_SIGN_P7);
     accelerationIMU.setMode(OPERATION_MODE_ACCONLY);
+    
+    
+    AInverse.cell(0, 0) = 0.985079;
+    AInverse.cell(0, 1) = -0.028604;
+    AInverse.cell(0, 2) = 0.003267;
+    AInverse.cell(1, 0) = -0.028604;
+    AInverse.cell(1, 1) = 0.990997;
+    AInverse.cell(1, 2) = 0.006620;
+    AInverse.cell(2, 0) = 0.003267;
+    AInverse.cell(2, 1) = 0.006620;
+    AInverse.cell(2, 2) = 0.996102;
 
-    adafruit_bno055_offsets_t orientationOffset{37, -51, 71, 35, -347, -224, -1, -2, -1, 1000, 1034}; //not using mag, but including anyway
-    orientationIMU.setSensorOffsets(orientationOffset);
+    bias(0) = 0.057610;
+    bias(1) = -0.209385;
+    bias(2) = 0.707400;
 
-    adafruit_bno055_offsets_t accelerationOffset{13, -17, -24, 0, 0, 0, 0, 0, 4, 1000, 0}; //not using mag, but including anyway
-    accelerationIMU.setSensorOffsets(accelerationOffset);
+
+    #if DEBUG
+        while(true){
+            readValues();
+            sPrintln(rawLocalAcceleration.z());
+            delay(100);
+        }
+    #endif
+
 
     dPrint("Orientation mode: "); dPrintln(orientationIMU.getMode());
     dPrint("Acceleration mode: "); dPrintln(accelerationIMU.getMode());
 
+    
     //calibrate();
 
     sPrintln("IMU Initialized");
 }
 
-void PhysicalIMU::calibrate(){
-    accelerationIMU.setMode(OPERATION_MODE_IMUPLUS);
-    uint8_t calibrationStatus, otherStatus;
-
-    //Gyro calibration (set still)
-    sPrintln("Calibration gyro");
-    do {
-        accelerationIMU.getCalibration(&otherStatus, &calibrationStatus, &otherStatus, &otherStatus);
-        sPrint(calibrationStatus);
+void PhysicalIMU::calibrateOrientationIMU(UI& ui){
+    sPrintln("Calibrating orientation IMU");
+    while(!ui.getButton()){
+        printAcceleration(orientationIMU);
+        sPrint(" ----- ");
+        printCalibration(orientationIMU);
+        sPrintln();
         delay(100);
-    } while(calibrationStatus != 3);
-    sPrintln("Gyro calibration complete");
-
-    sPrint("Calibrating accelerometer - ");
-
-    do{
-        accelerationIMU.getCalibration(&otherStatus, &otherStatus, &calibrationStatus, &otherStatus);
-        sPrint(calibrationStatus);
-        delay(100);
-    } while(calibrationStatus != 3);
-
-    //Magnetometer calibration wave around)
-    sPrintln("Calibrating magnetometer");
-    // do {
-    //     accelerationIMU.getCalibration(&otherStatus, &otherStatus, &otherStatus, &calibrationStatus);
-    //     delay(100);
-    // } while(calibrationStatus != 3);
-    sPrintln("Magnetometer calibration complete");
-
-    //Accelerometer calibration (move for each axis to sense earth)
-    
-
-
-    sPrintln("\nAccelerometer calibration complete");
-    adafruit_bno055_offsets_t offsets;
-    accelerationIMU.getSensorOffsets(offsets);
-    while(true){
-        sPrint("Accelerometer: ");
-        sPrint(offsets.accel_offset_x); sPrint(" ");
-        sPrint(offsets.accel_offset_y); sPrint(" ");
-        sPrint(offsets.accel_offset_z); sPrint(" ");
-
-        sPrint("\nGyro: ");
-        sPrint(offsets.gyro_offset_x); sPrint(" ");
-        sPrint(offsets.gyro_offset_y); sPrint(" ");
-        sPrint(offsets.gyro_offset_z); sPrint(" ");
-
-        sPrint("\nMag: ");
-        sPrint(offsets.mag_offset_x); sPrint(" ");
-        sPrint(offsets.mag_offset_y); sPrint(" ");
-        sPrint(offsets.mag_offset_z); sPrint(" ");
-
-        sPrint("\nAccel Radius: ");
-        sPrint(offsets.accel_radius);
-
-        sPrint("\nMag Radius: ");
-        sPrint(offsets.mag_radius);
-        delay(1000);
     }
-    // bno.setMode(OPERATION_MODE_IMUPLUS); //reset mode to not include magnetometer
+    printOffsets(orientationIMU);
+}
+
+void PhysicalIMU::calibrateAccelerationIMU(UI& ui){
+    while(true){
+        if(ui.getButton()){
+            delay(500); //half second to allow it to stabilize after button press 
+            imu::Vector<3> averageAcceleration;
+            const int DATA_POINTS = 100;
+            for(int i = 0; i < DATA_POINTS; i++){
+                readValues();
+                averageAcceleration = averageAcceleration + rawLocalAcceleration;
+                delay(50);
+            }
+            averageAcceleration = averageAcceleration / DATA_POINTS;
+
+            //Print to console
+            sPrint(averageAcceleration.x()); sPrint("\t"); 
+            sPrint(averageAcceleration.y()); sPrint("\t"); 
+            sPrint(averageAcceleration.z()); sPrint("\n");
+
+            ui.setTone(5000, 500);
+        } else
+            delay(100);
+    }
+}
+
+void PhysicalIMU::printAcceleration(Adafruit_BNO055& bno){
+    imu::Vector<3> acceleration = bno.getVector(Adafruit_BNO055::adafruit_vector_type_t::VECTOR_ACCELEROMETER);
+    sPrint("("); sPrint(acceleration.x()); sPrint(","); sPrint(acceleration.y()); sPrint(","); sPrint(acceleration.z()); sPrint(")");
+}
+
+void PhysicalIMU::printCalibration(Adafruit_BNO055& bno){
+    uint8_t systemCalib, gyroCalib, magCalib, accCalib;
+    bno.getCalibration(&systemCalib, &gyroCalib, &accCalib, &magCalib);
+    sPrint("system: "); sPrint(systemCalib); sPrint(", gyro: "); sPrint(gyroCalib); sPrint(", acc: "); sPrint(accCalib); sPrint(", mag: "); sPrint(magCalib);
+}
+
+void PhysicalIMU::printOffsets(Adafruit_BNO055& bno){
+  adafruit_bno055_offsets_t offsets;
+  bno.getSensorOffsets(offsets);
+
+  sPrint("Accelerometer: ");
+  sPrint(offsets.accel_offset_x); sPrint(" ");
+  sPrint(offsets.accel_offset_y); sPrint(" ");
+  sPrint(offsets.accel_offset_z); sPrint(" ");
+
+  sPrint("\nGyro: ");
+  sPrint(offsets.gyro_offset_x); sPrint(" ");
+  sPrint(offsets.gyro_offset_y); sPrint(" ");
+  sPrint(offsets.gyro_offset_z); sPrint(" ");
+
+  sPrint("\nMag: ");
+  sPrint(offsets.mag_offset_x); sPrint(" ");
+  sPrint(offsets.mag_offset_y); sPrint(" ");
+  sPrint(offsets.mag_offset_z); sPrint(" ");
+
+  sPrint("\nAccel Radius: ");
+  sPrint(offsets.accel_radius);
+
+  sPrint("\nMag Radius: ");
+  sPrint(offsets.mag_radius);
+  
+  sPrint("\n\n\n");
+  sPrint("{"); 
+  sPrint(offsets.accel_offset_x); sPrint(", "); 
+  sPrint(offsets.accel_offset_y); sPrint(", "); 
+  sPrint(offsets.accel_offset_z); sPrint(", "); 
+  sPrint(offsets.mag_offset_x); sPrint(", "); 
+  sPrint(offsets.mag_offset_y); sPrint(", "); 
+  sPrint(offsets.mag_offset_z); sPrint(", ");
+  sPrint(offsets.gyro_offset_x); sPrint(", ");
+  sPrint(offsets.gyro_offset_y); sPrint(", ");
+  sPrint(offsets.gyro_offset_z); sPrint(", "); 
+  sPrint(offsets.accel_radius); sPrint(", "); 
+  sPrint(offsets.accel_radius); sPrintln("}");
 }
 
 PhysicalIMU::PhysicalIMU(){}
 
 void PhysicalIMU::readValues(){
     quat = orientationIMU.getQuat();
-    //dPrintln(accelerationIMU.getMode());
-    rawLocalAcceleration = accelerationIMU.getVector(Adafruit_BNO055::adafruit_vector_type_t::VECTOR_ACCELEROMETER);
-    //dPrintln(rawLocalAcceleration.z());
+
+    //Apply accerleration calibration
+    imu::Matrix<3> unscaledLocalAcceleration;
+    unscaledLocalAcceleration.vector_to_col(accelerationIMU.getVector(Adafruit_BNO055::adafruit_vector_type_t::VECTOR_ACCELEROMETER)-bias, 0);
+    
+    rawLocalAcceleration = (AInverse * unscaledLocalAcceleration).col_to_vector(0);
+
+
+
     gravityLocalAcceleration = orientationIMU.getVector(Adafruit_BNO055::adafruit_vector_type_t::VECTOR_GRAVITY);
     localAcceleration = rawLocalAcceleration - gravityLocalAcceleration;
     globalAcceleration = quat.rotateVector(localAcceleration);
-    //sPrintln(globalAcceleration.z());
 }
 
 //Assumes values have been read already
--- Flight Code/src/hardware/IMU/PhysicalIMU.h
+++ Flight Code/src/hardware/IMU/PhysicalIMU.h
@@ -1,11 +1,22 @@
 #include <Adafruit_BNO055.h>
 #include <string>
 #include "../UI/UI.h"
+#include <BasicLinearAlgebra.h>
 
 class PhysicalIMU{
 private:    
     Adafruit_BNO055 orientationIMU{-1, 0x28}; //Maybe switch
     Adafruit_BNO055 accelerationIMU{-1, 0x29};
+
+    void printCalibration(Adafruit_BNO055&);
+    void printOffsets(Adafruit_BNO055&);
+    void printAcceleration(Adafruit_BNO055&);
+    void calibrateAccelerationIMU(UI&);
+    void calibrateOrientationIMU(UI&);
+
+    //Calibration constants
+    imu::Matrix<3> AInverse;
+    imu::Vector<3> bias;
     
 public: 
     PhysicalIMU();
